4be44d46aa2e1387d8ad6c91429a43462ceedb22
Erik Flores <erikfloresq@icloud.com>
2021-06-17 21:57:42 -0300

    Update api with async await

--- RickAndMorty15/Character/CharacterListViewModel.swift
+++ RickAndMorty15/Character/CharacterListViewModel.swift
@@ -12,24 +12,11 @@ import Combine
 class CharacterListViewModel: ObservableObject {
     let rickAndMortyAPI = RickAndMortyAPI()
     @Published var characters = [Character]()
-//    var cancellable = Set<AnyCancellable>()
-//
-//    func getCharacter() {
-//        rickAndMortyAPI
-//            .getCharacter()
-//            .map { $0.results }
-//            .sink { (completion) in
-//                print("")
-//            } receiveValue: { (characters) in
-//                self.characters = characters
-//            }.store(in: &cancellable)
-//    }
 
     func getCharacter() async throws {
         do {
-            let (data, error) = try await URLSession.shared.data(from: URL(string: "https://rickandmortyapi.com/api/character")!, delegate: nil)
-            let characters = try JSONDecoder().decode(ResponseAPI<Character>.self, from: data)
-            self.characters = characters.results
+            let resultApi = try await rickAndMortyAPI.getCharacter()
+            characters = resultApi.results
         } catch(let error) {
             throw error
         }
